7cf6c48319f57b74db84db8864ce1119e45cf44a
Hubert Targosz <targosz.hubert@gmail.com>
2021-09-17 16:09:47 +0200

    +2 classes, cinemamovie and main.

---
+++ src/main/java/pl/targosz/homework/CinemaMovie/CinemaMovie.java
@@ -0,0 +1,16 @@
+package pl.targosz.homework.CinemaMovie;
+
+public class CinemaMovie {
+    String title;
+    String name,surname;
+    int duration;
+    int productionYear;
+
+    public CinemaMovie(String title, String name, String surname, int duration, int productionYear) {
+        this.title = title;
+        this.name = name;
+        this.surname = surname;
+        this.duration = duration;
+        this.productionYear = productionYear;
+    }
+}
---
+++ src/main/java/pl/targosz/homework/CinemaMovie/Main.java
@@ -0,0 +1,7 @@
+package pl.targosz.homework.CinemaMovie;
+
+public class Main {
+    public static void main(String[] args) {
+        CinemaMovie movie = new CinemaMovie("Maciek z zielonego wzgórza","Quentin","Tarantino",90,2021);
+    }
+}
