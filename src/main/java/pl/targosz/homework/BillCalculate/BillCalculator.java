4c0eae7392d5118b0b9ebe1e5f861c6c3e66d2f7
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-05 16:23:00 +0200

    Oh my god it works (i hope) Added the method that has some arguments and now it actually works! (Billcalculating)

--- src/main/java/pl/targosz/homework/BillCalculate/BillCalculator.java
+++ src/main/java/pl/targosz/homework/BillCalculate/BillCalculator.java
@@ -1,15 +1,18 @@
 package pl.targosz.homework.BillCalculate;
 
 public class BillCalculator {
-    private static double calculate(double bill,double servicePayment) {
+    public static double calculate(double bill,double servicePayment) {
         return bill+servicePayment;
     }
 
-    protected static double calculate(double bill,double servicePayment, double discount) {
+    private static double calculate() {
+        double bill=15000;
+        double servicePayment=1500000;
+        double discount = 1500;
         return bill+servicePayment-discount;
     }
 
-    private static double calculate(protected double bill,protected  double servicePayment,protected double takeOutPayment) {
+    private static double calculate(double bill,double servicePayment,double takeOutPayment) {
         return bill+servicePayment+takeOutPayment;
     }
 }
