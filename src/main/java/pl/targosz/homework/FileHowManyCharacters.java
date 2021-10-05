21657bd51bf86262d4f618e06e59769d563858e1
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-05 18:50:05 +0200

    Didn`t end this one.

---
+++ src/main/java/pl/targosz/homework/FileHowManyCharacters.java
@@ -0,0 +1,10 @@
+package pl.targosz.homework;
+
+import java.io.File;
+
+public class FileHowManyCharacters {
+    public static void main(String[] args) {
+        File file = new File("E:\\KURS JAVA\\Workspaceofficial\\JavaRepository\\src\\main\\java\\pl\\targosz\\homework\\resources\\data.txt");
+        System.out.println(file);
+    }
+}
