fe6e8e9f591a143b17bd4a1378bd17eccfdac8ec
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-05 18:29:50 +0200

    Created a FileReading file that reads a normal file and then writes out a text on console.

---
+++ src/main/java/pl/targosz/homework/FileReading.java
@@ -0,0 +1,14 @@
+package pl.targosz.homework;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileReading {
+    public static void main(String[] args) throws IOException {
+        File file = new File("E:\\KURS JAVA\\Workspaceofficial\\JavaRepository\\src\\main\\java\\pl\\targosz\\homework\\resources\\data.txt");
+        String text = Files.readString(Path.of("E:\\KURS JAVA\\Workspaceofficial\\JavaRepository\\src\\main\\java\\pl\\targosz\\homework\\resources\\data.txt"));
+        System.out.println(text);
+    }
+}
