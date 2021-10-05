ff286fbd70f5182af9ab6e1cde741baee560452b
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-05 18:53:12 +0200

    The Class that writes out how many characters are in the file.

--- src/main/java/pl/targosz/homework/FileHowManyCharacters.java
+++ src/main/java/pl/targosz/homework/FileHowManyCharacters.java
@@ -5,6 +5,7 @@ import java.io.File;
 public class FileHowManyCharacters {
     public static void main(String[] args) {
         File file = new File("E:\\KURS JAVA\\Workspaceofficial\\JavaRepository\\src\\main\\java\\pl\\targosz\\homework\\resources\\data.txt");
-        System.out.println(file);
+        System.out.println(file.length());
+
     }
 }
