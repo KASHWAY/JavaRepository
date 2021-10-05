2f0e689a227f9d957731347bf77e1a8c10238a46
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-05 18:50:05 +0200

    Didn`t end this one.

--- src/main/java/pl/targosz/homework/FileHowManyCharacters.java
+++ src/main/java/pl/targosz/homework/FileHowManyCharacters.java
@@ -4,8 +4,7 @@ import java.io.File;
 
 public class FileHowManyCharacters {
     public static void main(String[] args) {
-        File file = new File("src/main/java/pl/targosz/homework/resources/data.txt");
-        System.out.println(file.length());
-
+        File file = new File("E:\\KURS JAVA\\Workspaceofficial\\JavaRepository\\src\\main\\java\\pl\\targosz\\homework\\resources\\data.txt");
+        System.out.println(file);
     }
 }
