e82b6a4f237c9a29f70d96ddbfa2caae54b80819
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-05 16:04:20 +0200

    Fixed the program i think.

--- src/main/java/pl/targosz/homework/Shape/Triangle.java
+++ src/main/java/pl/targosz/homework/Shape/Triangle.java
@@ -1,7 +1,13 @@
 package pl.targosz.homework.Shape;
 
 public class Triangle extends Shape{
-    double a,b,c,h;
+    protected double a,b,c,h;
+    public Triangle(double a,double b,double c,double h) {
+        this.a=a;
+        this.b=b;
+        this.c=c;
+        this.h=h;
+    }
     double getArea() {
         return a*h/2;
     }
