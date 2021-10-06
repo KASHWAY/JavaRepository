d5f0d1afca3dd40df081a6d2d98de53fee6bd145
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-06 12:10:56 +0200

    Created an enum and a class that takes that enum and returns a String text.

--- src/main/java/pl/targosz/homework/NamesOfWeekDays/Main.java
+++ src/main/java/pl/targosz/homework/NamesOfWeekDays/Main.java
@@ -1,45 +1,18 @@
 package pl.targosz.homework.NamesOfWeekDays;
 
 public class Main {
-    private static NamesOfWeekDays DayNumberConvertingToDayName(int number) {
-        switch (number) {
-            case 1:
-                return NamesOfWeekDays.PONIEDZIALEK;
+    public static NamesOfWeekDays YourWeekDay(NamesOfWeekDays weekDay) {
 
-            case 2:
-                return NamesOfWeekDays.WTOREK;
-
-            case 3:
-                return NamesOfWeekDays.SRODA;
-
-            case 4:
-                return NamesOfWeekDays.CZWARTEK;
-
-            case 5:
-                return NamesOfWeekDays.PIATEK;
-
-            case 6:
-                return NamesOfWeekDays.SOBOTA;
-
-            case 7:
-                return NamesOfWeekDays.NIEDZIELA;
-
-
-            default :
-                return NamesOfWeekDays.WRONGNUMBER;
-
-        }
-
+        return weekDay;
     }
+
     public static void main(String[] args) {
-        System.out.println(DayNumberConvertingToDayName(1));
-        System.out.println(DayNumberConvertingToDayName(2));
-        System.out.println(DayNumberConvertingToDayName(3));
-        System.out.println(DayNumberConvertingToDayName(4));
-        System.out.println(DayNumberConvertingToDayName(5));
-        System.out.println(DayNumberConvertingToDayName(6));
-        System.out.println(DayNumberConvertingToDayName(7));
-        System.out.println(DayNumberConvertingToDayName(10));
-        System.out.println(DayNumberConvertingToDayName(150));
+        System.out.println(NamesOfWeekDays.MONDAY);
+        System.out.println(NamesOfWeekDays.TUESDAY);
+        System.out.println(NamesOfWeekDays.WEDNESDAY);
+        System.out.println(NamesOfWeekDays.THURSDAY);
+        System.out.println(NamesOfWeekDays.FRIDAY);
+        System.out.println(NamesOfWeekDays.SATURDAY);
+        System.out.println(NamesOfWeekDays.SUNDAY);
     }
 }
