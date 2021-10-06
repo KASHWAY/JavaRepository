7125acc910c5ba879156d859a80fe3bea71bf805
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-06 18:28:06 +0200

    Changed for to while in this class. But still have some problems with it.

--- src/main/java/pl/targosz/homework/FileSecondLineWrite.java
+++ src/main/java/pl/targosz/homework/FileSecondLineWrite.java
@@ -11,12 +11,14 @@ public class FileSecondLineWrite {
     public static void main(String[] args) throws IOException {
         Path file = Paths.get("src/main/java/pl/targosz/homework/resources/data.txt");
         double lineCounter = Files.lines(file).count();
+        int n = 0;
 
-            for (int n=0;n<lineCounter;n+=2) {
-                String line = Files.readAllLines(Paths.get("src/main/java/pl/targosz/homework/resources/data.txt")).get(n);
-                System.out.println(line);
+        while (n < lineCounter) {
+            String line = Files.readAllLines(Paths.get("src/main/java/pl/targosz/homework/resources/data.txt")).get(n);
+            System.out.println(line);
+            n += 2;
+        }
 
-            }
 
     }
 }
