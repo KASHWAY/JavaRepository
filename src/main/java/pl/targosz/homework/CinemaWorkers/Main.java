4af1b1cf70de6052fec6a7b25d64610d39b26a5f
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-06 19:54:55 +0200

    Refactored the code so it looks better.

---
+++ src/main/java/pl/targosz/homework/CinemaWorkers/Main.java
@@ -0,0 +1,27 @@
+package pl.targosz.homework.CinemaWorkers;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("The base payment is : " + Employee.Payment + " dollars.");
+        System.out.println(("----------------------------------------"));
+
+        Manager manager = new Manager(2015, 1000);
+        manager.setName("Hubert");
+        manager.setSurname("Targosz");
+        manager.calculateMonthlySalary();
+        System.out.println("Your manager`s payment is : " + manager.calculateMonthlySalary() + " dollars.");
+        System.out.println("Your manager works : " + manager.workDuration() + " years ");
+
+        System.out.println(("----------------------------------------"));
+
+        TicketSeller ticketSeller = new TicketSeller(2018, 15);
+        ticketSeller.setName("Mateusz");
+        ticketSeller.setSurname("Mruwczynski");
+        ticketSeller.calculateMonthlySalary();
+        System.out.println("Your ticket seller got promoted and gets 1000 more, so his payment is : " + ticketSeller.calculateMonthlySalary() + "dollars");
+        System.out.println("Your ticket sellers work duration is : " + ticketSeller.workDuration() + " years");
+
+
+    }
+}
