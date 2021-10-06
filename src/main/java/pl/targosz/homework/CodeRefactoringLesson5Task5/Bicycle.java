b7406efeef1659d9aa9c968363cdc86e1b43ce28
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-06 18:18:07 +0200

    Refactored it correctly now.

--- src/main/java/pl/targosz/homework/CodeRefactoringLesson5Task5/Bicycle.java
+++ src/main/java/pl/targosz/homework/CodeRefactoringLesson5Task5/Bicycle.java
@@ -2,15 +2,6 @@ package pl.targosz.homework.CodeRefactoringLesson5Task5;
 
 public class Bicycle extends Vehicle {
 
-    protected void startEngine() {
-        throw new UnsupportedOperationException("Bicycle does not have engine");
-    }
-
-
-    protected void stopEngine() {
-        throw new UnsupportedOperationException("Bicycle does not have engine");
-    }
-
     @Override
     public boolean goingForward() {
         return true;
