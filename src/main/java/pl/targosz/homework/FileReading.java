ab37d12e5201e5b6ff310075d8df598da551b070
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-06 17:34:18 +0200

    This class reads a file`s text, changes it to uppercase and writes it to other file.

--- src/main/java/pl/targosz/homework/FileReading.java
+++ src/main/java/pl/targosz/homework/FileReading.java
@@ -1,13 +1,11 @@
 package pl.targosz.homework;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileReading {
     public static void main(String[] args) throws IOException {
-        File file = new File("src/main/java/pl/targosz/homework/resources/data.txt");
         String text = Files.readString(Path.of("src/main/java/pl/targosz/homework/resources/data.txt"));
         System.out.println(text);
     }
