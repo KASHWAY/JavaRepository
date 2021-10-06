15a5b5a16341650ce5344f0df5d38b2b48cc8a9c
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-06 13:58:57 +0200

    Refactoring my second code, and added 2 fancy methods!

---
+++ src/main/java/pl/targosz/homework/CodeRefactoringLesson5Task5/Vehicle.java
@@ -0,0 +1,6 @@
+package pl.targosz.homework.CodeRefactoringLesson5Task5;
+
+public abstract class Vehicle {
+    public abstract boolean goingForward();
+    public abstract boolean goingBackwards();
+}
