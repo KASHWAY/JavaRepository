0ff7f2559ad6e5bb1453eb54d11572c8ec1f909a
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-06 20:24:55 +0200

    Fixed it.

--- src/main/java/pl/targosz/homework/ConvertMinutesToSeconds.java
+++ src/main/java/pl/targosz/homework/ConvertMinutesToSeconds.java
@@ -6,6 +6,7 @@ public class ConvertMinutesToSeconds {
     }
     public static void main(String[] args) {
 
+
         System.out.println(ConvertMinutes(50));
         System.out.println(ConvertMinutes(120));
         System.out.println(ConvertMinutes(20));
