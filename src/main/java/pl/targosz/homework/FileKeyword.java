f1878787617e41699e7b0d166c1c3e4a0cdd0272
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-06 20:05:09 +0200

    Fixed it.

--- src/main/java/pl/targosz/homework/FileKeyword.java
+++ src/main/java/pl/targosz/homework/FileKeyword.java
@@ -8,9 +8,7 @@ import java.util.Scanner;
 
 public class FileKeyword {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
         String contains = Files.readString(Paths.get("src/main/java/pl/targosz/homework/resources/data.txt"));
         System.out.println(contains.contains("Kurs"));
-        scanner.close();
     }
 }
