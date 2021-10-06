3f474d7688624da8d9f1b1637ecfd8207820e5ba
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-06 12:55:35 +0200

    Didn`t fix it yet, need some help.

---
+++ src/main/java/pl/targosz/homework/UpgradedCalculator/CountingInterface.java
@@ -0,0 +1,5 @@
+package pl.targosz.homework.UpgradedCalculator;
+
+public interface CountingInterface {
+    double count(double number1,double number2);
+}
---
+++ src/main/java/pl/targosz/homework/UpgradedCalculator/UpgradedCalculator.java
@@ -0,0 +1,27 @@
+package pl.targosz.homework.UpgradedCalculator;
+
+public class UpgradedCalculator implements CountingInterface {
+
+    @Override
+    public double count(double number1, double number2) {
+
+        return number1 + number2;
+    }
+
+    public double count(double number1, double number2) {
+
+        return number1 - number2;
+    }
+
+    public double count(double number1, double number2) {
+
+        return number1 * number2;
+    }
+
+    public double count(double number1, double number2) {
+
+        return number1 / number2;
+    }
+
+
+}
