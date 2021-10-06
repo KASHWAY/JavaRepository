bbfa5b963569faa5375c2986d23901f2d7eea57d
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-06 18:34:28 +0200

    Fixed it.

--- src/main/java/pl/targosz/homework/FileReverse.java
+++ src/main/java/pl/targosz/homework/FileReverse.java
@@ -1,6 +1,5 @@
 package pl.targosz.homework;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,8 +7,6 @@ import java.nio.file.Paths;
 
 public class FileReverse {
     public static void main(String[] args) throws IOException {
-
-        File file = new File ("src/main/java/pl/targosz/homework/resources/data.txt");
         String text = Files.readString(Paths.get("src/main/java/pl/targosz/homework/resources/data.txt"));
         StringBuilder stringbuilder = new StringBuilder(text);
 
