1208d200112007c52deab24c7f72ce00b0848b90
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-07 13:12:33 +0200

    Created my first PeselValidator!

---
+++ src/main/java/pl/targosz/homework/ExceptionsPesel/IllegalLengthException.java
@@ -0,0 +1,7 @@
+package pl.targosz.homework.ExceptionsPesel;
+
+public class IllegalLengthException extends Exception {
+    public IllegalLengthException(String message) {
+        super(message);
+    }
+}
