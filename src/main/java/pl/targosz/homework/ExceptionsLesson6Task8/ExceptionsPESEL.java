83b49dd79b4ebb6bc007fa9562f0d3ecb4564b82
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-07 13:03:20 +0200

    My second task with Throwing exceptions!

---
+++ src/main/java/pl/targosz/homework/ExceptionsLesson6Task8/ExceptionsPESEL.java
@@ -0,0 +1,22 @@
+package pl.targosz.homework.ExceptionsLesson6Task8;
+
+import java.lang.invoke.WrongMethodTypeException;
+
+public class ExceptionsPESEL {
+    public static String peselCheck(String pesel, int length) throws IllegalLengthException {
+        if (pesel instanceof String) {
+            if (length!=11) {
+                throw new IllegalLengthException("It is not a Pesel!");
+            }
+        }
+        else
+            throw new WrongMethodTypeException("Beep, wrong type of a method.");
+    return pesel;
+    }
+
+    public static void main(String[] args) throws IllegalLengthException {
+        System.out.println(peselCheck("05252606514",11));
+        System.out.println(peselCheck("123",11));
+        System.out.println(peselCheck("12345678911",10));
+    }
+}
---
+++ src/main/java/pl/targosz/homework/ExceptionsLesson6Task8/IllegalLengthException.java
@@ -0,0 +1,7 @@
+package pl.targosz.homework.ExceptionsLesson6Task8;
+
+public class IllegalLengthException extends Exception {
+   public IllegalLengthException(String message) {
+       super(message);
+   }
+}
