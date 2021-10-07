034196ee919e73a5360b09148ea27fae158d71aa
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-07 13:17:56 +0200

    Created a Class that Expcets the Pesel and throws illegal argument if something isn`t correct.

---
+++ src/main/java/pl/targosz/homework/ExceptionsPesel/ExceptionsPesel.java
@@ -0,0 +1,22 @@
+package pl.targosz.homework.ExceptionsPesel;
+
+import java.lang.invoke.WrongMethodTypeException;
+
+public class ExceptionsPesel {
+    public static String peselCheck(String pesel, int length) throws IllegalLengthExceptions {
+        if (pesel instanceof String) {
+            if (length!=11) {
+                throw new IllegalLengthExceptions("It is not a Pesel!");
+            }
+        }
+        else
+            throw new WrongMethodTypeException("Beep, wrong type of a method.");
+        return pesel;
+    }
+
+    public static void main(String[] args) throws IllegalLengthExceptions {
+        System.out.println(peselCheck("05252606514",11));
+        System.out.println(peselCheck("123",11));
+        System.out.println(peselCheck("12345678911",10));
+    }
+}
---
+++ src/main/java/pl/targosz/homework/ExceptionsPesel/IllegalLengthExceptions.java
@@ -0,0 +1,7 @@
+package pl.targosz.homework.ExceptionsPesel;
+
+public class IllegalLengthExceptions extends Exception {
+    public IllegalLengthExceptions(String message) {
+        super(message);
+    }
+}
