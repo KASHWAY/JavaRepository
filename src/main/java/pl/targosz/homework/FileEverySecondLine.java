ffacd0ca11750763ad2d039dbc2a865af19b70e5
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-12 21:19:11 +0200

    Created a class that reads every second file.

---
+++ src/main/java/pl/targosz/homework/FileEverySecondLine.java
@@ -0,0 +1,22 @@
+package pl.targosz.homework;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileEverySecondLine {
+
+    public static void main(String[] args) throws IOException, IOException {
+        int n = 0;
+        List<String> line = Files.readAllLines(Paths.get("src/main/java/pl/targosz/homework/resources/data.txt"));
+        while (n < line.size()) {
+
+            System.out.println(line.get(n));
+            n += 2;
+        }
+
+
+    }
+
+}
