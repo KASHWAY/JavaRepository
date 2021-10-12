847499cbfeb69ef712f437f685ede5c6f4a3871a
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-12 21:09:36 +0200

    Created a class that checks if the word is a Pesel.

---
+++ src/main/java/pl/targosz/homework/PeselExceptions/IllegalLengthExceptions.java
@@ -0,0 +1,7 @@
+package pl.targosz.homework.PeselExceptions;
+
+public class IllegalLengthExceptions extends Exception {
+    public IllegalLengthExceptions(String message) {
+        super(message);
+    }
+}
