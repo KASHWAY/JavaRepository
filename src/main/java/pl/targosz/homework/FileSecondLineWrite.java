e07bb81059b221928b9af3e846cb8389cb750b8e
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-12 21:14:53 +0200

    Created a class that reads every second line.

--- src/main/java/pl/targosz/homework/FileSecondLineWrite.java
+++ src/main/java/pl/targosz/homework/FileSecondLineWrite.java
@@ -1,11 +1,9 @@
 package pl.targosz.homework;
 
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-
 
 public class FileSecondLineWrite {
     public static void main(String[] args) throws IOException {
