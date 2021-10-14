44887ccd61e10f7e9f267aee2be9ab53d678f734
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-14 11:52:32 +0200

    Created a class and a test, with MethodSource test.

---
+++ src/main/java/pl/targosz/homework/ReturningTextLength.java
@@ -0,0 +1,11 @@
+package pl.targosz.homework;
+
+public class ReturningTextLength {
+    public static int textLength(String text) {
+        return text.length();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(textLength("Sigib"));
+    }
+}
