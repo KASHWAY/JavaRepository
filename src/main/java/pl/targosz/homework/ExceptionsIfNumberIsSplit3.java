4a694643fdee3defdffa743ec652d9e727a574ff
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-14 13:30:22 +0200

    Have no clue how to do it haha.

--- src/main/java/pl/targosz/homework/ExceptionsIfNumberIsSplit3.java
+++ src/main/java/pl/targosz/homework/ExceptionsIfNumberIsSplit3.java
@@ -1,21 +1,24 @@
 package pl.targosz.homework;
 
 public class ExceptionsIfNumberIsSplit3 {
-    public static void validate(int value) {
-        if (value % 3 != 0)
+    public static void validate(int[] values) {
+        if (values[0] % 3 != 0)
             throw new IllegalArgumentException();
         else
+
             System.out.println("Your number can be split through 3!");
     }
 
     public static void main(String[] args) {
         try {
-            validate(5);
+            validate(new int[]{5});
         } catch (Exception e) {
             System.out.println("Exception caught!");
             e.printStackTrace();
         } finally {
-            validate(3);
+            for (int value : values){
+                System.out.println(value);
+            }
         }
     }
 
