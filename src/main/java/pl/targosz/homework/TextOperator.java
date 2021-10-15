1bd1a368e384b2bd4fe704c1e975d6a40dd09d48
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-15 21:35:16 +0200

    Created a program that operates on a text, checks if there are any chars, if there is at least 1, then it checks if the text is a palindrome.

--- src/main/java/pl/targosz/homework/TextOperator.java
+++ src/main/java/pl/targosz/homework/TextOperator.java
@@ -1,9 +1,33 @@
 package pl.targosz.homework;
 
 public class TextOperator {
+    public static boolean isPalindrome(String text) {
+        int length = text.length();
+        int forward = 0, backward = length - 1;
+        while (backward > forward) {
+            char forwardChar = text.charAt(forward++);
+            char backwardChar = text.charAt(backward--);
+            if (forwardChar != backwardChar)
+                return false;
+
+
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
-        String text="To moj tekst!";
+        String text = "To mój tekst";
+        String[] someArray = text.split("");
+        for (int i = 0; i < someArray.length; i++) {
+            if (someArray[i].contains("")) {
+                System.out.println("There is a char!");
+                System.out.println("Text length : " + text.length());
+                break;
+            }
+            else
+                    System.out.println("There is no characters!");
+        }
+        System.out.println(isPalindrome(text));
 
     }
 }
