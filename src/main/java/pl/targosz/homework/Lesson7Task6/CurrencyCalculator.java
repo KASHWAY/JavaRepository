eee93490c3942a791edf95b9ca7e4a857c94e82d
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-16 11:27:27 +0200

    Perfect.

--- src/main/java/pl/targosz/homework/Lesson7Task6/CurrencyCalculator.java
+++ src/main/java/pl/targosz/homework/Lesson7Task6/CurrencyCalculator.java
@@ -3,20 +3,21 @@ package pl.targosz.homework.Lesson7Task6;
 import java.math.BigDecimal;
 
 public class CurrencyCalculator {
-    private final CurrencyInterface currencyInterface;
+    private static CurrencyInterface currencyInterface;
 
     public CurrencyCalculator(CurrencyInterface currencyInterface) {
         this.currencyInterface = currencyInterface;
     }
 
     public BigDecimal sellRateCalculator(BigDecimal PLN, Currency currency) {
-        BigDecimal sellRate = CurrencyInterface.sellRate(currency);
+        BigDecimal sellRate = currencyInterface.sellRate(currency);
         return sellRate.multiply(PLN);
 
     }
 
+
     public BigDecimal buyRateCalculator(BigDecimal PLN, Currency currency) {
-        BigDecimal buyRate = CurrencyInterface.buyRate(currency);
+        BigDecimal buyRate = currencyInterface.buyRate(currency);
         return buyRate.multiply(PLN);
     }
 }
--- src/main/java/pl/targosz/homework/Lesson7Task6/CurrencyInterface.java
+++ src/main/java/pl/targosz/homework/Lesson7Task6/CurrencyInterface.java
@@ -4,11 +4,7 @@ package pl.targosz.homework.Lesson7Task6;
 import java.math.BigDecimal;
 
 public interface CurrencyInterface {
-    static BigDecimal buyRate(Currency currency) {
-        return null;
-    }
+    BigDecimal buyRate(Currency currency);
 
-    static BigDecimal sellRate(Currency currency) {
-        return null;
-    }
+    BigDecimal sellRate(Currency currency);
 }
