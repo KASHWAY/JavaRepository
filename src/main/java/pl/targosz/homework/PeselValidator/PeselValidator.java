d9933c70deb8a4a2589603a18a05e9824455f120
Hubert Targosz <targosz.hubert@gmail.com>
2021-10-17 13:02:07 +0200

    Created a Pesel Validator, that Validates pesel`s length and if pesel has only numbers.

--- src/main/java/pl/targosz/homework/PeselValidator/PeselValidator.java
+++ src/main/java/pl/targosz/homework/PeselValidator/PeselValidator.java
@@ -1,27 +1,27 @@
 package pl.targosz.homework.PeselValidator;
 
-public class PeselValidator {
-    private char PESEL[] = new char[11];
 
-        PESEL[1]=='a';
-    public PeselValidator(String peselNumber) {
-        if (peselNumber.length() != 11) {
-            valid = false;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PeselValidator {
+
+    private static boolean isPeselSizeValid(String text) {
+
+        Pattern pattern = Pattern.compile("\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d");
+        for (int i = 0; i < 11; i++) {
+            Matcher matcher = pattern.matcher(text);
+            boolean matches = matcher.matches();
+            return matches;
         }
 
-    }
-
-    static boolean ifPeselIsANumber(String PESEL[]) {
-        int ascii = (int) PESEL[1];
-        System.out.println(ascii);
-
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(ifPeselIsANumber());
+        System.out.println(isPeselSizeValid("12345632110"));
+        System.out.println(isPeselSizeValid("ABCDEFGH"));
     }
-
-
 }
 
 
